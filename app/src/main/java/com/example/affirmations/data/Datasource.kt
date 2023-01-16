5dac89cac6d58ff9c34c336d8c95e0d42f8c0eac
Boitshoko <boitshoko@glucode.com>
2023-01-16 16:04:46 +0200

    complete

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -20,7 +20,7 @@ import com.example.affirmations.R
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    /*fun loadAffirmations(): List<Affirmation> {
+    fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
@@ -32,5 +32,5 @@ class Datasource() {
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
             Affirmation(R.string.affirmation10, R.drawable.image10))
-    }*/
+    }
 }
